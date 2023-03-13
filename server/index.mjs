f21dab2372ce99f9f2226ace4c66c0c30222a4f7
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-03-13 15:04:19 -0300

    include new routers

--- server/index.mjs
+++ server/index.mjs
@@ -2,7 +2,9 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
-import usuarios from "./routes/posts.mjs";
+import usuarios from "./routes/usuarios.mjs";
+import departamentos from "./routes/departamentos.mjs";
+import departamentosFuncoes from "./routes/departamentos-funcoes.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -10,8 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Load the /posts routes
+// Usuários
 app.use("/usuarios", usuarios);
+// Departamentos
+app.use("/departamentos", departamentos);
+// Departamentos
+app.use("/departamentos-funcoes", departamentosFuncoes);
 
 // Global error handling
 app.use((err, _req, res, next) => {
