b53420a734a712a2323e90c2c5dd1b8b0f76260b
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-05-25 16:29:09 -0300

    atualizando rotas com match

--- server/routes/sys-eventos-inscritos.mjs
+++ server/routes/sys-eventos-inscritos.mjs
@@ -5,7 +5,7 @@ import logsFunction from "../logs/logs-functions.mjs";
 
 const router = express.Router();
 
-//Recuperar Ingressos
+//Recuperar Ingressos por Usuário
 router.post("/", async (req, res) => {
   let collection = await db.collection("sys-eventos-inscritos");
   // console.log(req);
@@ -19,6 +19,7 @@ router.post("/", async (req, res) => {
   if (id_usuario != null) {
     ingressos = await collection
       .aggregate([
+        { $match : { id_usuario : id_usuario } },
         { $addFields: { id: { $toObjectId: id_usuario } } },
         { $addFields: { id2: { "$toObjectId": "$id_ingresso" } } },
         {
@@ -46,6 +47,84 @@ router.post("/", async (req, res) => {
   if (!ingressos) res.send(error).status(404);
   else res.send(ingressos).status(200);
 });
+
+
+//Recuperar Ingressos por ID
+router.post("/inscricao", async (req, res) => {
+    let collection = await db.collection("sys-eventos-inscritos");
+    // console.log(req.body.id);
+    let id = ObjectId(req.body.id) ;
+    let ingressos = {};
+    let usuarios = {};
+    // console.log(result.length > 0);
+  
+    if (id != null) {   
+      ingressos = await collection
+        .aggregate([
+          { $match : { _id : id } },
+          { $addFields: { id: { "$toObjectId": "$id_usuario" } } },
+          { $addFields: { id2: { "$toObjectId": "$id_ingresso" } } },
+          {
+            $lookup: {
+              from: "usuarios",
+              localField: "id",
+              foreignField: "_id",
+              as: "USUARIO",
+            },
+          },
+          {
+              $lookup: {
+                from: "sys-eventos-ingressos",
+                localField: "id2",
+                foreignField: "_id",
+                as: "INGRESSO",
+              },
+            },
+        ])
+        .toArray();
+    }
+    let error = {};
+  //   ingressos = {usuarios};
+  //   console.log(ingressos);
+    if (!ingressos) res.send(error).status(404);
+    else res.send(ingressos).status(200);
+  });
+
+//Recuperar todos os Ingressos
+router.get("/", async (req, res) => {
+    let collection = await db.collection("sys-eventos-inscritos");
+    let ingressos = {};  
+      ingressos = await collection
+        .aggregate([
+          { $addFields: { id: { "$toObjectId": "$id_usuario" } } },
+          { $addFields: { id2: { "$toObjectId": "$id_ingresso" } } },
+          {
+            $lookup: {
+              from: "usuarios",
+              localField: "id",
+              foreignField: "_id",
+              as: "USUARIO",
+            },
+          },
+          {
+              $lookup: {
+                from: "sys-eventos-ingressos",
+                localField: "id2",
+                foreignField: "_id",
+                as: "INGRESSO",
+              },
+            },
+        ])
+        .toArray();
+    let error = {};
+  //   ingressos = {usuarios};
+  //   console.log(ingressos);
+    if (!ingressos) res.send(error).status(404);
+    else res.send(ingressos).status(200);
+  });
+
+
+
 
 //Recuperar Eventos
 router.post("/meus-eventos", async (req, res) => {
