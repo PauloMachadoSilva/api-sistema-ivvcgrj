8437ea9bbff89e393824095e177732c68fec1e09
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-05-30 17:06:53 -0300

    ajuste nos tokens

--- server/environments/environment.mjs
+++ server/environments/environment.mjs
@@ -3,8 +3,10 @@ export const environment = {
     apiProd: 'https://api.verbocampogranderj.com.br',
     apiWhatsApp: 'https://api-whats.verbocampogranderj.com.br', 
     pagSeguroSandBox: {
-        contaEmail: 'pauloems@yahoo.com.br',
-        token: 'D79B66FD29C546CAB1AF08215369573B',
+        // PAULO contaEmail: 'pauloems@yahoo.com.br',
+        contaEmail: 'juniorjosiasf@gmail.com',
+        // PAULO token: 'D79B66FD29C546CAB1AF08215369573B',
+        token: '68887C2C9A6A412DB8F32B0EE886D8C6',
         criarSecao: 'https://ws.sandbox.pagseguro.uol.com.br/v2/sessions', //POST
         obterTokenCartao: 'https://df.uol.com.br/v2/cards', //POST
         obterBandeiraCartao: 'https://df.uol.com.br/df-fe/mvc/creditcard/v1/getBin', //GET
@@ -18,8 +20,10 @@ export const environment = {
         cartaoTesteCVV:'123'
     },
     pagSeguroProd: {
-        contaEmail: 'pauloems@yahoo.com.br',
-        token: '0347E203BE8F4E019D3310CE5368DAC0',
+        // contaEmail: 'pauloems@yahoo.com.br',
+        contaEmail: 'juniorjosiasf@gmail.com',
+        // token: '0347E203BE8F4E019D3310CE5368DAC0',
+        token: '19d7d467-6d0a-48bd-992a-2e36da857d1ce72df45040c199104818c34ed31d9ff0d0fe-7c7b-4b43-b9bc-1e0ef5e29e8d',
         criarSecao: 'https://ws.pagseguro.uol.com.br/v2/sessions', //POST
         obterTokenCartao: 'https://df.uol.com.br/v2/cards', //POST
         obterBandeiraCartao: 'https://df.uol.com.br/df-fe/mvc/creditcard/v1/getBin', //GET
