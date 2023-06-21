59c2834eb0446e2dd95f5db8601434a042c6f3af
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-06-21 01:56:30 -0300

    pix e notificacao

--- server/routes/sys-eventos-notificacoes.mjs
+++ server/routes/sys-eventos-notificacoes.mjs
@@ -7,11 +7,15 @@ import { environment } from "../environments/environment.mjs";
 const router = express.Router();
 var obj;
 //RECUPERAR NOTIFICAÇÕES
+
+router.use(express.urlencoded({extended:false}));
 router.post("/", async (req, res) => {
+    // console.log('req>>>',req)
+    // console.log('res>>>',res)
     console.log('req>>>',req)
-    console.log('res>>>',res)
+    console.log('body>>>',req.body.notificationCode)
     return;
-    let notificacao = String(req.params.notificationCode);
+    let notificacao = String(req.body.notificationCode);
     const options = {
         headers: {accept: 'application/xml'}
       };
