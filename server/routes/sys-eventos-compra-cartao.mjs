6132f1a872fd90ce4e8a34f63196caa1b6028bec
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-06-26 16:57:33 -0300

    cartao

--- server/routes/sys-eventos-compra-cartao.mjs
+++ server/routes/sys-eventos-compra-cartao.mjs
@@ -93,6 +93,7 @@ router.post("/", async (req, res) => {
     .then(async function (response) {
       
       let tokenXml = convert2.convertXML(response.data);
+      console.log('tokenXml',tokenXml)
       let tokenCartao = tokenXml.card.children[0].token.content;
 
       // console.log('tokenCartao>',tokenCartao.card.children[0].token.content);
