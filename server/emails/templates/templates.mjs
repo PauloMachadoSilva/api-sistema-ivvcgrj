4e9cd7353398d9f27cd8184764e38044559412e6
Paulo Machado <sistemas.ivvcgrj@gmail.com>
2023-08-11 01:49:54 -0300

    feat: ajuste nas apis

--- server/emails/templates/templates.mjs
+++ server/emails/templates/templates.mjs
@@ -6,7 +6,7 @@ export default function htmlTemplates (dadosIngresso, qrcodes) {
         <body>
             <img style='width:280px' src='https://verbocampogranderj.com.br/assets/imgs/logo-verbo-cg.png'>
             <h1>Compra aprovada! Seus ingressos já estão disponíveis.</h1>
-            <h2>${dadosIngresso.INGRESSO[0].titulo}</h2>
+            <h2>${dadosIngresso[0].INGRESSO[0].titulo}</h2>
             <h3>Igreja Verbo da Vida em Campo Grande RJ</h3>
             <div>${qrcodes}</div>
             <div style='margin-top:24px'>
