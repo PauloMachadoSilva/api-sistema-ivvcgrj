551de36a37f78cea47278fce8a7b9dfc5f16cbfc
PauloMachadoSilva <36408029+PauloMachadoSilva@users.noreply.github.com>
2024-02-07 16:09:50 -0300

    feat: ajuste

--- server/routes/sys-eventos-ingressos.mjs
+++ server/routes/sys-eventos-ingressos.mjs
@@ -1,7 +1,7 @@
 import express from "express";
 import db from "../db/conn.mjs";
 import { ObjectId } from "mongodb";
-import logsFunctionAdm from "../logs/logs-functions-adm.mjs";
+import logsFunction from "../logs/logs-functions.mjs";
 
 const router = express.Router();
 
@@ -26,10 +26,10 @@ router.post("/", async (req, res) => {
   });
 
   //Recuperar Ingressos
-router.post("/todos", async (req, res) => {
+router.post("/", async (req, res) => {
   let collection = await db.collection("sys-eventos-ingressos");
   // console.log(req);
-  let query = {_id: ObjectId(req.body.id_ingresso)};
+  let query = {id_evento: String(req.body.id_evento), ativo : true, online : true};
   let result = await collection.find(query).sort({ tipo:1, valor : 1, data: 1 }).toArray();
   let error = {}
   if (!result) res.send(error).status(404);
@@ -106,73 +106,20 @@ router.get("/ingressos-eventos", async (req, res) => {
 //Inserir Evento POST
 router.post("/incluir-ingressos", async (req, res) => {
   let collection = await db.collection("sys-eventos-ingressos");
-  // console.log(req.body.ingresso);
+  // console.log(req.body);
   // return;
-  let query = req.body.ingresso;
+  let query = req.body;
   query.data = new Date(query.data);
   query.limite = Number(query.limite);
   query.tipo_sistema = 'sys-eventos';
   // console.log(query);
   // return;
   let result = await collection.insertOne(query);
-  let log = req.body.usuario;
-  log.acao = "cadastrar-ingressos";
-  log.nome_collection = "sys-eventos-ingressos";
-  log.id_collection = result.insertedId;
-  log.metodo = "post";
-  log.campos = req.body.ingresso
-  log.tipo = 0; // 0: Não gera notificação 1: Gera notificação
-  log.data = new Date();
-  logs(log);
-  // return;
   let error = {};
   if (!result) res.send(error).status(404);
   else res.send(result).status(200);
 });
 
-//Alterar Evento POST
-router.post("/atualizar-ingressos/:id", async (req, res) => {
-  let collection = await db.collection("sys-eventos-ingressos");
-  const query = { _id: ObjectId(req.params.id) };
-  const updates = {
-    $set: {
-        titulo: req.body.ingresso.titulo,
-        descricao: req.body.ingresso.descricao,
-        data: new Date(req.body.ingresso.data),
-        id_evento: req.body.ingresso.id_evento,
-        observacao: req.body.ingresso.observacao,
-        valor: req.body.ingresso.valor,
-        tipo: 3,
-        limite: Number(req.body.ingresso.limite),
-        link_externo: req.body.ingresso.link_externo,
-        desconto_progressivo: req.body.ingresso.desconto_progressivo,
-        ativo: req.body.ingresso.ativo,
-        online: req.body.ingresso.online,
-    }
-  }
-  // console.log('query', query)
-  // console.log('updates', updates)
-  // return;
-  let result = await collection.updateOne(query, updates);
 
-  //logs
-  let log = req.body.usuario;
-  log.acao = "atualizar-ingressos";
-  log.nome_collection = "sys-eventos-ingressos";
-  log.id_collection = result;
-  log.metodo = "post";
-  log.campos = req.body.ingresso
-  log.tipo = 0; // 0: Não gera notificação 1: Gera notificação
-  log.data = new Date();
-  logs(log);
-  // return;
-  let error = {};
-  if (!result) res.send(error).status(404);
-  else res.send(result).status(200);
-});
-
-async function logs(log) {
-  let log_result = await logsFunctionAdm(log);
-}
 
 export default router;
