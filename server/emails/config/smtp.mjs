cd3ca9b7c6a69cb14503e4c0980b6db92c179b14
PauloMachadoSilva <36408029+PauloMachadoSilva@users.noreply.github.com>
2026-07-17 11:57:41 -0300

    Update smtp.mjs

--- server/emails/config/smtp.mjs
+++ server/emails/config/smtp.mjs
@@ -3,6 +3,6 @@
     port: 587,
     secure: false, // true for 465, false for other ports
     user: 'sistemas.ivvcgrj@gmail.com', // generated ethereal user
-    pass: 'wfix csuz ptrs xqjr', // generated ethereal password
+    pass: 'mkcm afgg teae vkul', // generated ethereal password
     email: 'sistemas.ivvcgrj@gmail.com'
  }
