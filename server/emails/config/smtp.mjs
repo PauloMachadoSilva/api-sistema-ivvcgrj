e823e18952f7ec970f5fa36a8031f6af1f3564eb
PauloMachadoSilva <pauloems@yahoo.com.br>
2026-09-03 22:29:28 -0300

    feat: nova pwd app

--- server/emails/config/smtp.mjs
+++ server/emails/config/smtp.mjs
@@ -3,6 +3,6 @@
     port: 587,
     secure: false, // true for 465, false for other ports
     user: 'contato@verbocampogranderj.com.br', // generated ethereal user
-    pass: 'nunj bjys bcoj zgnq', // generated ethereal password
+    pass: 'mlwa dkxz kezv hxpf', // generated ethereal password
     email: 'contato@verbocampogranderj.com.br'
  }
